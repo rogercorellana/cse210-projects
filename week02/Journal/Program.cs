e1822d4c8a09d662eafd3010d2a1ade65c2a93da
Roger Camacho <roger.camacho@mitrol.net>
2026-07-16 13:53:38 -0300

    changes

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,71 @@
-using System;
-
+// EXCEEDING REQUIREMENTS:
+// 1. I added a "Mood" variable in the Entry class to save how the user was feeling that day.
+// 2. I used a custom separator (~~) for saving and loading the text file. 
+// This way, the user can type commas or quotes in their answers and the file won't break.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        bool keepRunning = true;
+
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while (keepRunning)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+                
+                Console.Write("What is your current mood? ");
+                string mood = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry._date = DateTime.Now.ToShortDateString();
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+                newEntry._mood = mood;
+
+                journal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == "5")
+            {
+                keepRunning = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        }
     }
 }
